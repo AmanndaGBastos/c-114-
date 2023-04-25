1d157fa1274559cfee610f17ebf400f73b17951b
AmanndaGBastos <105891173+AmanndaGBastos@users.noreply.github.com>
2023-04-25 19:16:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,218 @@
+noseX=0
+noseY=0
+function preload() {
+    img=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+    }
+    
+    function setup() {
+    canvas=createCanvas(300,300);
+  canvas.center()
+  video=createCapture(VIDEO)
+  poseNet=ml5.poseNet(video,modelLoaded)
+  poseNet.on('pose', gotPoses)
+}
+function modelLoaded(){
+console.log("PoseNet is initialized now")
+}
+function gotPoses(results){
+  if(results.length>0){
+console.log(results)
+ noseX=results[0].pose.nose.x-15
+ noseY=results[0].pose.nose.y-15
+  }
+}
+    
+    function draw() {
+      image(video,0,0,300,300);
+      image(clown_nose,noseX,noseY,30,30);
+    }
+    function takeSnapshot(){
+        save("Palhacito.png")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*noseX=0;
+  noseY=0;
+
+  function preload() {
+    clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+  }
+
+  function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+
+  function modelLoaded() {
+    console.log('PoseNet foi inicializado');
+  }
+
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      noseX = results[0].pose.nose.x-15;
+      noseY = results[0].pose.nose.y-15;
+    }
+  }
+
+  function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clownNose, noseX, noseY, 30, 30);
+  }
+
+  function takeSnapshot(){    
+    save('myFilterImage.png');
+  }*/
